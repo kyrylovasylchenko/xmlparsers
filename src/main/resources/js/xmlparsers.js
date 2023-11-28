ebef5aad405cd0664ea5e7a8378959561214d99e
kyva22 <tyrtyrforit@gmail.com>
2023-11-28 22:27:28 +0200

    added flags while adding/deleting projects

--- src/main/resources/js/xmlparsers.js
+++ src/main/resources/js/xmlparsers.js
@@ -2,46 +2,8 @@ AJS.toInit(function ($){
     $("#scripts").auiSelect2();
 
 
-    $.ajax({
-        type: "GET",
-        url: AJS.contextPath() + "/rest/xmlparsers/1.0/project",
-        success: function (data){
-            var dataArray = JSON.parse(data);
+    uploadProjects()
 
-            var tableBody = document.getElementById("tableBody");
-            var projectSelect = document.getElementById("projectSelect");
-            var dataListElement = projectSelect.querySelector("datalist");
-
-            dataArray.forEach(function(item) {
-                var row = tableBody.insertRow();
-
-                var projectNameCell = row.insertCell(0);
-                projectNameCell.innerHTML = item.projectName;
-
-                var bassProjectKeyCell = row.insertCell(1);
-                bassProjectKeyCell.innerHTML = item.bassProjectKey;
-
-                var targetProjectKeyCell = row.insertCell(2);
-                targetProjectKeyCell.innerHTML = item.targetProjectKey;
-
-                var scriptsCell = row.insertCell(3);
-                scriptsCell.innerHTML = item.scripts.join(", ");
-
-                var deleteButtonCell = row.insertCell(4);
-                var deleteButton = document.createElement("button");
-                deleteButton.innerHTML = "<span class=\"aui-icon aui-icon-small aui-iconfont-trash\" role=\"img\" aria-label=\"Insert meaningful text here for accessibility\" />";
-                deleteButtonCell.appendChild(deleteButton);
-                deleteButton.onclick = function(e) {
-                    e.preventDefault();
-                    deleteProject(item.projectName);
-                };
-                var listElement = document.createElement("aui-option");
-                listElement.innerHTML = item.projectName
-                dataListElement.appendChild(listElement)
-
-            });
-        }
-    })
 
     $('#projectForm').on('aui-valid-submit',function (e){
         e.preventDefault();
@@ -60,16 +22,28 @@ AJS.toInit(function ($){
             contentType: "application/json",
             data: JSON.stringify(projectData),
             success: function (data){
-                if(data === ""){
-                    $('#success-massage .success-message-body').text('Macros to change was not found');
-                }else{
-                    $('#success-massage .success-message-body').text(data);
-                }
-                console.log(data)
-                $("#success-massage").fadeIn();
+                var flag = AJS.flag({
+                    type: 'success',
+                    body: 'Project has been added',
+                });
+                setTimeout(() => {
+                    flag.close()
+                }, 3 * 1000)
+                deleteProject()
+                uploadProjects()
+            },
+            error:function (){
+                var flag = AJS.flag({
+                    type: 'error',
+                    body: 'Project has not been added',
+                });
+                setTimeout(() => {
+                    flag.close()
+                }, 3 * 1000)
             }
         })
     })
+
 
     $('#cleaningTask').on('aui-valid-submit',function (e){
         e.preventDefault();
@@ -97,6 +71,8 @@ AJS.toInit(function ($){
 
 })
 
+
+
 function deleteProject(projectName){
     var messageElement = document.getElementById("dialog-project")
     messageElement.innerHTML = projectName
@@ -116,8 +92,72 @@ function deleteProject(projectName){
             url: AJS.contextPath() + "/rest/xmlparsers/1.0/project",
             data: {
                 projectName:projectName
+            },
+            success: function (data){
+                var table = document.getElementById("tableBody");
+                var rows = table.getElementsByTagName("tr");
+                for (var i = 0; i < rows.length; i++) {
+                    var cells = rows[i].getElementsByTagName("td");
+                    if (cells.length > 0 && cells[0].innerHTML === projectName) {
+                        table.deleteRow(i);
+                        break;
+                    }
+                }
+                var flag = AJS.flag({
+                    type: 'success',
+                    body: 'Project has been deleted',
+                });
+                setTimeout(() => {
+                    flag.close()
+                }, 3 * 1000)
             }
         })
         AJS.dialog2("#project-remove-dialog").hide();
     });
 }
+
+
+function uploadProjects(){
+    AJS.$.ajax({
+        type: "GET",
+        url: AJS.contextPath() + "/rest/xmlparsers/1.0/project",
+        success: function (data){
+            var dataArray = JSON.parse(data);
+
+            var tableBody = document.getElementById("tableBody");
+            var projectSelect = document.getElementById("projectSelect");
+            var dataListElement = projectSelect.querySelector("datalist");
+
+            dataArray.forEach(function(item) {
+                var row = tableBody.insertRow();
+
+                var projectNameCell = row.insertCell(0);
+                projectNameCell.innerHTML = item.projectName;
+
+                var bassProjectKeyCell = row.insertCell(1);
+                bassProjectKeyCell.innerHTML = item.bassProjectKey;
+
+                var targetProjectKeyCell = row.insertCell(2);
+                targetProjectKeyCell.innerHTML = item.targetProjectKey;
+
+                var scriptsCell = row.insertCell(3);
+                scriptsCell.innerHTML = item.scripts.join(", ");
+
+                var deleteButtonCell = row.insertCell(4);
+                var deleteButton = document.createElement("button");
+                deleteButton.className = "aui-button aui-button-danger";
+                deleteButton.innerHTML = "<span class=\"aui-icon aui-icon-small aui-iconfont-trash\" role=\"img\" aria-label=\"Insert meaningful text here for accessibility\" />";
+                deleteButtonCell.appendChild(deleteButton);
+                deleteButton.onclick = function(e) {
+                    e.preventDefault();
+                    deleteProject(item.projectName);
+                };
+                var listElement = document.createElement("aui-option");
+                listElement.innerHTML = item.projectName
+                dataListElement.appendChild(listElement)
+
+            });
+        }
+    })
+}
+
